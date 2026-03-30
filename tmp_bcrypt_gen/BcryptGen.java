c540f405698bb17cc41ea82bd5ed31e435d0db74
chyun7114 <chyun7114@naver.com>
2026-03-31 00:03:48 +0900

    임시 bcrypt 파일 정리 및 gitignore 추가

--- tmp_bcrypt_gen/BcryptGen.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-public class BcryptGen {
-  public static void main(String[] args) {
-    BCryptPasswordEncoder e = new BCryptPasswordEncoder();
-    System.out.println("password=" + e.encode("password"));
-    System.out.println("seedpass=" + e.encode("seedpass"));
-  }
-}
